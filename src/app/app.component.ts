a7000b54dd20af3a1bbc43a2eaca6f70af7b0a5d
Masudur Rahman <masudur.rahman@kaz.com.bd>
2018-12-18 18:01:25 +0600

    used setValue and patchValue of the form object in TD form approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,35 @@ export class AppComponent {
   defaultQuestion = 'teacher';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    userName: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    /** setValue to set/overwrite values of the whole form */
+    /* this.signupForm.setValue({
+      userData: {
+        username: suggestedName,
+        email: ''
+      },
+      secret: 'pet',
+      questionAnswer: '',
+      gender: 'male'
+    }); */
+
+    /** patchValue of form object to set/overwrite specific values of the form */
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   /* onSubmit(form: NgForm) {
@@ -21,6 +47,14 @@ export class AppComponent {
   } */
 
   onSubmit() {
+    this.submitted = true;
     console.log(this.signupForm);
+    this.user.userName = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
